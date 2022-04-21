5eec842cfbd4628d376c74db8a017f8a8d609000
eniskastrati <eniskastrati18@gmail.com>
2022-04-21 23:41:26 +0200

    #comment react class components learn

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-//this is how we have to import it now after not exported as default
+import Welcome from './components/Welcome';
 import { Greet } from './components/Greet';
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
 }
