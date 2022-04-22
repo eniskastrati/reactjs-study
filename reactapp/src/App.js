8cdfb33a6c5033ce60282ff98fdd2740c5c916dd
Enis Kastrati <enis@smartmile.eu>
2022-04-22 13:43:50 +0200

    #comment learned JSX components

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 //this is how we have to import it now after not exported as default
 import { Greet } from './components/Greet';
+import Hello from './components/Hello'
 
 function App() {
   return (
     <div className="App">
-      <Greet />
+      {/* <Greet /> */}
+      <Hello />
     </div>
   );
 }
---
+++ reactapp/src/components/Hello.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello JSX Component</h1>
+        </div>
+    );
+}
+
+export default Hello;
