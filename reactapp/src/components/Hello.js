dc2b1eb615dc8a851c8c04c555d8cad9ec848983
Enis Kastrati <enis@smartmile.eu>
2022-04-22 13:49:23 +0200

    #comment creating a hellocomponent without JSX

--- reactapp/src/components/Hello.js
+++ reactapp/src/components/Hello.js
@@ -1,10 +1,20 @@
-import react from 'react';
+import React from 'react';
 
 const Hello = () => {
-    return (
-        <div>
-            <h1>Hello JSX Component</h1>
-        </div>
+    // return (
+    //     <div>
+    //         <h1>Hello JSX Component</h1>
+    //     </div>
+    // );
+
+    return React.createElement(
+        'div',
+        {id: 'hello', class: 'dummyClass'},
+        React.createElement(
+            'h1',
+            null,
+            'Hello Enis'
+        )
     );
 }
 
