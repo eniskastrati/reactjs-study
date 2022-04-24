cf53ef3a84d0897efb7d232b6ff5c2ad5045b756
eniskastrati <eniskastrati18@gmail.com>
2022-04-24 16:38:17 +0200

    #comment learing to set state and to change it in class components

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+
 import Welcome from './components/Welcome';
-import { Greet } from './components/Greet';
+import Greet from './components/Greet';
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
+      {/* <Greet />
+      <Welcome name="Enis" heroName="https://eniskastrati.de" /> */}
+      <Message />
     </div>
   );
 }
