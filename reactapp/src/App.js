2fc7abdcbd4a7c0b3998e360c75baa0449d24a09
eniskastrati <eniskastrati18@gmail.com>
2022-04-25 00:20:32 +0200

    #comment methods as props

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -8,11 +8,14 @@ import Counter from './components/Counter';
 import Click from './components/Click';
 import ClassClick from './components/ClassClick';
 import EvenBind from './components/EvenBind';
+import ParentComponent from './components/ParentComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <EvenBind />
+      {/* <EvenBind /> */}
+      <ParentComponent />
       {/* <ClassClick /> */}
       {/* <Greet name={"enis"} heroName={"Kastrati"} /> */}
       {/* <Welcome name="Enis" heroName="Spiderman" /> */}
