f841fe74c259f9b50c0c0ae9d2d356e34e0c81d8
eniskastrati <eniskastrati18@gmail.com>
2022-04-25 16:52:15 +0200

    #comment inline styling in react

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -11,11 +11,13 @@ import ParentComponent from './components/ParentComponent';
 import UserGreeting from "./components/UserGreeting";
 import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
 
 function App() {
   return (
     <div className="App">
-      <Stylesheet primary={true} />
+      <Inline />
+      {/* <Stylesheet primary={true} /> */}
       {/* <NameList /> */}
       {/* <EvenBind /> */}
       {/* <UserGreeting />
