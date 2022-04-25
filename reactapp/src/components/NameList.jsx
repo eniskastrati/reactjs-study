1584fa383218bd3aa494795ba049cf6875632d16
eniskastrati <eniskastrati18@gmail.com>
2022-04-25 15:09:04 +0200

    #comment list rendering from an array with objects with different parameters

--- reactapp/src/components/NameList.jsx
+++ reactapp/src/components/NameList.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
 function NameList() {
-    const nameArray = ['Diana', 'Clarck', 'Bruce']
-    const nameList = nameArray.map((name,idx) => <h1 id={idx}>{name}</h1>);
-  return (<div>{nameList}</div>)
+    const persons = [
+      {
+        id: 0,
+        name: 'Bruce',
+        age: 25,
+        skill: 'Reactjs'
+      },
+      {
+        id: 1,
+        name: 'Keit',
+        age: 32,
+        skill: 'Angular'
+      },
+      {
+        id: 2,
+        name: 'Angeline',
+        age: 20,
+        skill: 'Golang'
+      },
+      {
+        id: 3,
+        name: 'Tobi',
+        age: 40,
+        skill: 'Vue'
+      },
+    ]
+    const personsList = persons.map((person) => <h1 id={person.id}>I am {person.name}. I am {person.age} years old. <br /> My Skills are : {person.skill} <hr /></h1>);
+  return (<div>{personsList}</div>)
 }
 
 export default NameList
