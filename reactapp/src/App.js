2c4ea1c5dcf3f6ebabbd6eb4f9167becd3e9b550
eniskastrati <eniskastrati18@gmail.com>
2022-04-25 17:02:42 +0200

    #comment styling with css and css modules

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -12,11 +12,17 @@ import UserGreeting from "./components/UserGreeting";
 import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
+import './appStyle.css'
+import styles from './appStyles.module.css'
+
+
 
 function App() {
   return (
     <div className="App">
-      <Inline />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      {/* <Inline /> */}
       {/* <Stylesheet primary={true} /> */}
       {/* <NameList /> */}
       {/* <EvenBind /> */}
