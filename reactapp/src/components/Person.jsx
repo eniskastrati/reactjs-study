bdad31df7307615a3663d3bbc3df45e7268fc93c
eniskastrati <eniskastrati18@gmail.com>
2022-04-25 15:31:52 +0200

    #commet removing useless code

--- reactapp/src/components/Person.jsx
+++ reactapp/src/components/Person.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Person({person}) {
   return (
-    <h1 key={person.id}>
+    <h1>
         I am {person.name}. I am {person.age} years old.
         <br /> My Skills are : {person.skill} <hr />
      </h1>
