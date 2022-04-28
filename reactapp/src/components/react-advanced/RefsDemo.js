686165c0772870989c4aaba1a493729f7c33e215
eniskastrati <eniskastrati18@gmail.com>
2022-04-28 22:33:23 +0200

    #comment changed input type

--- reactapp/src/components/react-advanced/RefsDemo.js
+++ reactapp/src/components/react-advanced/RefsDemo.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react'
 
 class RefsDemo extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.inputRef = React.createRef()
-      this.cbRef = null
-      this.setcbRef = (element) => {
-        this.cbRef = element
-      }
-    }
+  constructor(props) {
+    super(props)
 
-    componentDidMount() {
-        if(this.cbRef) {
-            this.cbRef.focus()
-        }
-        // this.inputRef.current.focus();
-        // console.log(this.inputRef)
-        
+    this.inputRef = React.createRef()
+    this.cbRef = null
+    this.setcbRef = (element) => {
+      this.cbRef = element
     }
-    clickHandler = () => {
-        alert(this.inputRef.current.value)
+  }
+
+  componentDidMount() {
+    if (this.cbRef) {
+      this.cbRef.focus()
     }
+    // this.inputRef.current.focus();
+    // console.log(this.inputRef)
+
+  }
+  clickHandler = () => {
+    alert(this.inputRef.current.value)
+  }
   render() {
     return (
       <div>
-          <input type="text" ref={this.inputRef}/>
-          <input type="text" ref={this.setcbRef}/>
-          <button onClick={this.clickHandler}>Click</button>
+        <input type="text" ref={this.inputRef} />
+        <input type="text" ref={this.setcbRef} />
+        <button onClick={this.clickHandler}>Click</button>
       </div>
     )
   }
